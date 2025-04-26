df58b011303677950613529ca0f51882faa203e4
Zaira Acosta <zacosta@mvs.com>
2025-04-26 16:06:13 -0600

    Corrección de ortografía, Comparación con Bubblesort modificado

--- bubblesMod.py
+++ bubblesMod.py
@@ -16,15 +16,36 @@ import time
 # Cuando termina el recorrido, revisa si la variable de control indica cambios, si hubo cambios quiere decir que puede haber mas.
 # Recorre tantas veces sea necesario el arreglo hasta que la variable de control no indica cambios y se concluye que se han
 # ordenado todos los números
+
+
+# Se programó una propuesta encontrada en:
+# Sharma, V. (2015). A new approach to improve worst case efficiency of Bubble Sort. International Research Journal of Computer Science (IRJCS), 2(6), 54–58. https://www.researchgate.net/publication/313647211_A_New_Approach_to_Improve_Worst_Case_Efficiency_of_Bubble_Sort
+# En escencia se propone un preordenamiento de la lista de datos y luego se aplica el método de burbuja convencional.
+# Revisando esta propuesta acorta notablemente el tiempo de ejecución de los algoritmos
+
+def preOrderBubble(arrPO):
+    arrPO = arrPO[::-1]  # Se invierte la Lista
+    front = 0
+    last = len(arrPO) - 1
+
+    while front < last:  # tomar el primer elemento y el último del arreglo y compararlos.
+        if arrPO[front] > arrPO[last]: # Si el primero es mayor que el último intercambia
+            arrPO[front], arrPO[last] = arrPO[last], arrPO[front]
+        
+        front += 1 # Avanza en el siguiente elemento
+        last -= 1 # Retrocede al anterior elemento
+      #Se cicla hasta que se llega a los elementos centrales
+
+    return arrPO
+
+
 def bubbles( numArr ):
   ctrl = 1
   while ctrl > 0:
     ctrl = 0
     for i in range(len(numArr) - 1):
       if numArr[i] > numArr[i+1]:
-        aux = numArr[i]
-        numArr[i]=numArr[i+1]
-        numArr[i+1]=aux
+        numArr[i], numArr[i+1] = numArr[i+1], numArr[i]
         ctrl = 1
   return numArr
 
@@ -50,26 +71,47 @@ for i in range(10000):
 
 
 
-
-
 # Se ejecuta bubles en los 3 arreglos:
 #CASO 1
 t0 = time.time()
 ordenado = bubbles(arrC1)
 tf = time.time()
-print(f"\nTiempo total Caso 1: {tf - t0:.5f} segundos")
+print(f"\nTiempo total Caso 1 Burbuja Normal: {tf - t0:.5f} segundos")
 print("Arreglo ordenado:"); print(ordenado[:20], "...")
+
+t0 = time.time()
+preOrd = preOrderBubble(arrC1)
+ordenado = bubbles(preOrd)
+tf = time.time()
+print(f"\nTiempo total Caso 1 Burbuja Modificada: {tf - t0:.5f} segundos")
+print("Arreglo ordenado:"); print(ordenado[:20], "...")
+
 
 #CASO 2
 t0 = time.time()
 ordenado = bubbles(arrC2)
 tf = time.time()
-print(f"\nTiempo total Caso 2: {tf - t0:.5f} segundos")
+print(f"\nTiempo total Caso 2 Burbuja Normal: {tf - t0:.5f} segundos")
 print("Arreglo ordenado:"); print(ordenado[:20], "...")
+
+t0 = time.time()
+preOrd = preOrderBubble(arrC2)
+ordenado = bubbles(arrC2)
+tf = time.time()
+print(f"\nTiempo total Caso 2 Burbuja Modificada: {tf - t0:.5f} segundos")
+print("Arreglo ordenado:"); print(ordenado[:20], "...")
+
 
 #CASO 3
 t0 = time.time()
 ordenado = bubbles(arrC3)
 tf = time.time()
-print(f"\nTiempo total Caso 3: {tf - t0:.5f} segundos")
+print(f"\nTiempo total Caso 3 Burbuja Normal: {tf - t0:.5f} segundos")
+print("Arreglo ordenado:"); print(ordenado[:20], "...")
+
+t0 = time.time()
+preOrd = preOrderBubble(arrC3)
+ordenado = bubbles(arrC3)
+tf = time.time()
+print(f"\nTiempo total Caso 3 Burbuja Modificada: {tf - t0:.5f} segundos")
 print("Arreglo ordenado:"); print(ordenado[:20], "...")
